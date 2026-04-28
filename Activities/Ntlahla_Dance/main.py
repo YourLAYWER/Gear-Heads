14775cbccd546ed567571d8704ca872e6f3aab6c
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-04-28 07:51:27 +0200

    Added new files

---
+++ Activities/Ntlahla_Dance/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import GyroSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+ev3 = EV3Brick()
+
+"""
+We start by testing the speaker
+"""
+try: 
+    ev3.speaker.beep()
+
+wait(3000)
+
+#---------------------------------
+# MUSIC PLAYS WHILE IT MOVES
+#------------------
+
+ev3.speaker.play_file("music.wav" wait=False
+
+#-----------------------
+# Watch me move Watch me nay nay music
+# -----------------------
+
+
---
+++ Activities/Ntlahla_Dance/test_mucsic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import GyroSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+
+ev3 = EV3Brick()
+
+#Test sound
+
+try: 
+    ev3.speaker.play_file("music.mp3")
