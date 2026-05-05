2be776b8d13c1322c2e5cc063f863717f1475b09
Mpilwenhle Ngwenya <mteegaby@gmail.com>
2026-05-05 12:05:45 +0200

    reworked a majority of my code in an attempt to improve functionality across the board

--- Activities/functionality.py
+++ Activities/functionality.py
@@ -5,134 +5,125 @@ from pybricks.ev3devices import GyroSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.ev3devices import Motor
-from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import ColorSensor
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait
 
 #Constants First
-DRIVE_SPEED = 30     
+DRIVE_SPEED = 30  #Measured in mm/s   
 PROPORTIONAL_GAIN = 1.8 #Determines wobbliness/sluggishness of the bot.
+LIFT_UP_ANGLE = 90     # The absolute angle representing the "up" position
+LIFT_DOWN_ANGLE = 0    # The absolute angle representing the "down" position
 
 #Variables
+ev3 = EV3Brick()
 gyro = GyroSensor(Port.S1)
 line_sensor = ColorSensor(Port.S3)
 lift_motor = Motor(Port.A)
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
+robot = DriveBase(left_motor, right_motor, 56, 121)
+wheel_diameter = 56      
+axle_track = 121
+distance_covered = 0 #Initialize distance(To be used as a measurement at multiple points during operation)
+robot.settings(straight_speed=200, turn_rate=90)
 
-#First block of code is intended to reset and initialise the bot itself.
-
-ev3 = EV3Brick()
-
+#First block of code is intended to reset the gyro sensor.
+#Commented out gyro lines because I'm not sure of their necessity at the moment.
 '''
 gyro.reset_angle(0)
 print(gyro.angle())
 wait(1000)
 '''
-#print("Start")
 
 ##INITIALISATION STARTS
+#Set Black
+ev3.screen.clear()
+ev3.screen.draw_text(0, 20, "Place on BLACK")
+ev3.screen.draw_text(0, 50, "Press any btn") 
+while len(ev3.buttons.pressed()) == 0:
+wait(10)
+black_value = line_sensor.reflection()
+ev3.speaker.beep(500, 200)
 
-#Prep & initialise the color sensor.
-def set_color():
-    robot = DriveBase(left_motor, right_motor, 56, 121) 
+#Debounce command follows, ensures that the user isn't pressing anything
+while len(ev3.buttons.pressed()) > 0:  
+wait(10)
 
-    #Set Black
-    ev3.screen.clear()
-    ev3.screen.draw_text(0, 20, "Place on BLACK")
-    ev3.screen.draw_text(0, 50, "Press any btn") 
-    while len(ev3.buttons.pressed()) == 0:
-        wait(10)
-    black_value = line_sensor.reflection()
-    ev3.speaker.beep(500, 200)
+#Set White
+ev3.screen.clear()
+ev3.screen.draw_text(0, 20, "Place on WHITE")
+ev3.screen.draw_text(0, 50, "Press any btn")
+while len(ev3.buttons.pressed()) == 0:
+wait(10)
+white_value = line_sensor.reflection()
+ev3.speaker.beep(1000, 200)
 
-    #Debounce command follows, ensures that the user isn't pressing anything
-    while len(ev3.buttons.pressed()) > 0:  
-        wait(10)
+#Debounce again
+while len(ev3.buttons.pressed()) > 0:
+wait(10)
+return black_value, white_value
 
-    #Set White
-    ev3.screen.clear()
-    ev3.screen.draw_text(0, 20, "Place on WHITE")
-    ev3.screen.draw_text(0, 50, "Press any btn")
-    while len(ev3.buttons.pressed()) == 0:
-        wait(10)
-    white_value = line_sensor.reflection()
-    ev3.speaker.beep(1000, 200)
-
-    #Debounce again
-    while len(ev3.buttons.pressed()) > 0:
-        wait(10)
-
-    #Calculations for initialization
-    TARGET_THRESHOLD = (black_value + white_value) / 2
-
-
-
+#Calculations for initialization
+TARGET_THRESHOLD = (black_value + white_value) / 2
 
 # Display results
 ev3.screen.clear()
 ev3.screen.draw_text(0, 10, "Blk: " + str(black_value))
 ev3.screen.draw_text(0, 30, "Wht: " + str(white_value))
-ev3.screen.draw_text(0, 60, "Thr: " + str(TARGET_THRESHOLD))
-ev3.screen.clear()
-#ev3.screen.draw_text("Calibration Complete. Threshold: ", TARGET_THRESHOLD)
-wait(3000) 
-
-ev3.screen.clear()
-#ev3.screen.draw_text("Got here lol")
-
+ev3.screen.draw_text(0, 60, "Thr: " + str(TARGET_THRESHOLD)) 
+print("Calibration Complete. Threshold:", TARGET_THRESHOLD)
+wait(3000)
 ##INITIALISATION DONE
 
-#ev3.screen.draw_text("Lowering the bar")
-
 #Lower the bar 100% 
-lift_motor.run_target(100, -1)
+ev3.screen.draw_text(0, 30, "Lowering the bar")
+lift_motor.reset_angle(0)
+lift_down()
 
-#Next block follows the line forward until the end.(Using color sensor)
-ev3.speaker.beep()
-ev3.screen.clear()
-ev3.screen.draw_text(0, 50, "Following Line...")
-current_reflection = line_sensor.reflection()
-error = current_reflection - TARGET_THRESHOLD
-steering = error * PROPORTIONAL_GAIN
-robot.drive(DRIVE_SPEED, steering)
-wait(50)
-#ev3.screen.draw_text("Time to TOW!")
+#Move toward the car
+move(1000)
+distance_covered += 1000
 
 #Raise the bar 100%
-lift_motor.reset_angle(0)
-lift_motor.run_target(100, 110)
+ev3.screen.draw_text(0, 30, "Raising The bar")
+lift_up()
 
+#Turn Towards the road
+turn(-90)
+distance_covered +- 90
+
+#Next block follows the line forward until the end.(Using color sensor)
+def color_move(distance_covered):    
+    while True:
+        if(distance_covered == 1500):
+            break      
+        ev3.speaker.beep()
+        ev3.screen.clear()
+        ev3.screen.draw_text(0, 50, "Following Line...")
+        current_reflection = line_sensor.reflection()
+        error = current_reflection - TARGET_THRESHOLD
+        steering = error * PROPORTIONAL_GAIN
+        robot.drive(DRIVE_SPEED, steering)
+        wait(50)
+        distance_covered += 1
+
+color_move(distance_covered)
+
+#Lower the bar 100% (Car drop off)
+ev3.screen.draw_text(0, 30, "Lowering the bar")
+lift_down()
+
+#Move back (Out of the way fo the car)
+move(-400)
 #Turn Around (180 Degrees)
-#Kinematic setup for accuracy first.
-wheel_diameter = 56      
-axle_track = 121      
+turn(180)
 
+#Follow the line back (To the pick up point)
+trip_two_distance = 0 #To be used as the new distance_covered variable
+color_move(trip_two_distance)
 
-#Actual movement
-ev3.speaker.beep()
-sequence_count = 2 #Changed sequence count from 3 to 1 to turn once, monitor this effect
-#Chaned to 2 to see if it has any effect on functioning
-for i in range(sequence_count):
-    print("Sequence " + str(i + 1) + ": Turning 180 degrees Right (Clockwise)")
-    robot.turn(180)
-    wait(1000)
-ev3.speaker.beep(frequency=1000, duration=500) #Higher pitched beep indicates completion.
+#Beep to indicate the end of the road
 
-#Follow the line back (To the drop off point)
-ev3.speaker.beep()
-ev3.screen.clear()
-ev3.screen.draw_text(0, 50, "Following Line...")
-current_reflection = line_sensor.reflection()
-error = current_reflection - TARGET_THRESHOLD
-steering = error * PROPORTIONAL_GAIN
-robot.drive(DRIVE_SPEED, steering)
-wait(50)
-#ev3.screen.draw_text("Drop off time.")
-
-#Lower the bar 100%
-lift_motor.run_target(100, -1)
 
 #Implementation of victory sound will follow. Still yet to find a sound to use.
 
