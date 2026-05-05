79f78c726998da3fceeabeac59fd4de8aa3390d4
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-05-05 08:29:28 +0200

    Added new dra and drop. detect using ultrasonic sensor

---
+++ Activities/Ntlahla_Dance/dragitems.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait
+
+ev3 = EV3Brick()
+
+# MOTOR SETUP
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+lift_motor = Motor(Port.A)  
+
+robot = DriveBase(left_motor, right_motor, 56, 121)
+
+# SENSOR SETUP
+line_sensor = ColorSensor(Port.S3)
+ultrasonic = UltrasonicSensor(Port.S4)
+gyro_sensor = GyroSensor(Port.S1)
+
+
+# -------------------------
+# LIFT FUNCTIONS
+# -------------------------
+
+def lower_lever():
+    lift_motor.run_angle(200, -90)
+
+def raise_lever():
+    lift_motor.run_angle(200, 90)
+
+# -------------------------
+# OBJECT DETECTION
+# -------------------------
+
+def object_in_front():
+    distance = ultrasonic.distance() 
+
+    if distance < 150:
+        return True
+    else:
+        return False
+
+# -------------------------
+# MAIN PROGRAM
+# -------------------------
+
+raise_lever()
+ev3.speaker.beep()
+
+while True:
+
+    robot.drive(100, 0)
+
+    if object_in_front():
+        robot.stop()
+        ev3.speaker.beep()
+
+        robot.straight(80)
+
+        lower_lever()
+        wait(500)
+
+        robot.straight(-500)   
+
+        robot.stop()
+        ev3.speaker.beep()
+
+        raise_lever()
+
+        break
+
+    wait(50)
