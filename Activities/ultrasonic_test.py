c0e4f6fe001528387bb9275d88a3e6306f2b3c7a
MrBoardWalker <s228131715@mandela.ac.za>
2026-05-15 15:28:01 +0200

    increased gains

--- Activities/ultrasonic_test.py
+++ Activities/ultrasonic_test.py
@@ -31,13 +31,13 @@ gyro_sensor= GyroSensor(Port.S1)
 
 ####################### Here is where my code starts ############################
 DRIVE_SPEED = 30
-TARGET_DISTANCE = 100
+TARGET_DISTANCE = 150
 SPEED_GAIN = 1.8
 
-COLOR_GAIN = 1.3
+COLOR_GAIN = 1.8
 THRESHOLD = 5
 WIDTH = 400
-LENGTH = 500
+LENGTH = 450
 
 # def measure_grey():
 #     ev3.screen.clear()
