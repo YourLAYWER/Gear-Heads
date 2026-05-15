9270b3bbc6d379cf2a7b42a3f054e5b5f4166719
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-05-15 15:14:17 +0200

    mmf

--- Activities/Ntlahla_Dance/search&follow.py
+++ Activities/Ntlahla_Dance/search&follow.py
@@ -24,120 +24,16 @@ robot = DriveBase(left_motor, right_motor, 56, 121)
 ultrasonic = UltrasonicSensor(Port.S4)
 touch_sensor = TouchSensor(Port.S2)
 
-#-----------------------------
-# TESTING
-#----------------------------
-# ev3.speaker.beep()
 
-# # Move forward
-# robot.straight(300)
-# wait(500)
+ev3.speaker.beep()
 
-# # Move backward
-# robot.straight(-300)
-# wait(500)
+# Move forward
+robot.straight(300)
+wait(500)
 
-# robot.stop()
-# ev3.speaker.beep()
+# Move backward
+robot.straight(-300)
+wait(500)
 
-
-# -------------------------
-# MODES / SETTINGS
-# -------------------------
-
-follow_mode = False
-return_mode = False
-
-TARGET_DISTANCE = 200
-MOVE_SPEED = 120
-RELEASE_DISTANCE = 120
-
-
-# -------------------------
-# FUNCTIONS
-# -------------------------
-
-def push_release_tool():
-    robot.straight(60)
-    wait(300)
-
-    robot.straight(-70)
-    wait(300)
-
-
-def turn_around():
-    robot.turn(180)
-
-
-def follow_target():
-    distance = ultrasonic.distance()
-
-    if distance > TARGET_DISTANCE:
-        robot.drive(MOVE_SPEED, 0)
-    else:
-        robot.stop()
-
-
-def collect_items():
-    global return_mode
-
-    ev3.speaker.say("Collecting")
-
-    for count in range(3):
-        push_release_tool()
-
-    ev3.speaker.say("Returning")
-
-    turn_around()
-
-    return_mode = True
-
-
-# -------------------------
-# MAIN LOOP
-# -------------------------
-
-while True:
-
-    # Touch sensor stops everything
-    if touch_sensor.pressed():
-
-        follow_mode = not follow_mode
-
-        if follow_mode:
-            return_mode = False
-            ev3.speaker.say("Follow mode")
-        else:
-            return_mode = False
-            ev3.speaker.say("Stopped")
-            robot.stop()
-
-        while touch_sensor.pressed():
-            wait(10)
-
-    # -------------------------
-    # FOLLOW TO COLLECTION POINT
-    # -------------------------
-
-    if follow_mode:
-
-        distance = ultrasonic.distance()
-
-        if distance > TARGET_DISTANCE:
-            robot.drive(MOVE_SPEED, 0)
-        else:
-            robot.stop()
-
-        if distance < RELEASE_DISTANCE:
-            robot.stop()
-            follow_mode = False
-            collect_items()
-
-    # -------------------------
-    # FOLLOW BACK TO BASE
-    # -------------------------
-
-    if return_mode:
-        follow_target()
-
-    wait(50)
+robot.stop()
+ev3.speaker.beep()
