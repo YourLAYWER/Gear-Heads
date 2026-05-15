05ae88a56daa36533de9466aec0037b2113761ba
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-05-15 18:24:58 +0200

    mnm

--- Activities/Ntlahla_Dance/search&follow.py
+++ Activities/Ntlahla_Dance/search&follow.py
@@ -1,41 +1,52 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor
+from pybricks.ev3devices import Motor, TouchSensor, UltrasonicSensor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 
 ev3 = EV3Brick()
-#----------------------------------
+
 # Motors
-#-----------------------------
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 robot = DriveBase(left_motor, right_motor, 56, 121)
 
+# Sensors
 touch_sensor = TouchSensor(Port.S2)
+ultrasonic = UltrasonicSensor(Port.S4)
+
+# Distance limit (mm)
+STOP_DISTANCE = 200
 
 ev3.speaker.say("Press to start")
 
-# Wait for first press
+# Wait for button
 while not touch_sensor.pressed():
     wait(10)
 
-# Small delay so one press is not counted twice
 wait(500)
 
 ev3.speaker.say("Moving")
 
-# Start driving forever
+# Start moving
 robot.drive(150, 0)
 
-# Wait until button pressed again
-while not touch_sensor.pressed():
-    wait(10)
+while True:
 
-# Stop robot
-robot.stop()
+    distance = ultrasonic.distance()
 
-ev3.speaker.say("Stopped")
+    print(distance)
+
+    # Object detected close
+    if distance < STOP_DISTANCE:
+
+        robot.stop()
+
+        ev3.speaker.say("Object")
+
+        break
+
+    wait(50)
