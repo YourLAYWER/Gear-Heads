918a22e56164b343529e6a7a07fffe7b4dd1e7ef
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-05-19 15:07:49 +0200

    kdsfhj

--- Activities/Ntlahla_Dance/search&follow.py
+++ Activities/Ntlahla_Dance/search&follow.py
@@ -8,39 +8,21 @@ from pybricks.tools import wait
 
 ev3 = EV3Brick()
 
-# -------------------------
-# MOTORS
-# -------------------------
-
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-lift_motor = Motor(Port.A)
 
 robot = DriveBase(left_motor, right_motor, 56, 121)
-
-# -------------------------
-# SENSORS
-# -------------------------
 
 touch_sensor = TouchSensor(Port.S2)
 ultrasonic = UltrasonicSensor(Port.S4)
 gyro_sensor = GyroSensor(Port.S1)
 
-# -------------------------
-# SETTINGS
-# -------------------------
-
 TARGET_DISTANCE = 180
 MAX_DISTANCE = 600
 MOVE_SPEED = 80
 
-press_count = 0
 follow_mode = False
 
-
-# -------------------------
-# FUNCTIONS
-# -------------------------
 
 def gyro_turn(target_angle):
     gyro_sensor.reset_angle(0)
@@ -64,7 +46,6 @@ def search_left_right():
     robot.stop()
     wait(500)
 
-    # look left
     gyro_turn(-20)
 
     distance = ultrasonic.distance()
@@ -72,7 +53,6 @@ def search_left_right():
     if TARGET_DISTANCE < distance < MAX_DISTANCE:
         return
 
-    # look right
     gyro_turn(40)
 
     distance = ultrasonic.distance()
@@ -80,7 +60,6 @@ def search_left_right():
     if TARGET_DISTANCE < distance < MAX_DISTANCE:
         return
 
-    # return to center
     gyro_turn(-20)
 
 
@@ -98,97 +77,18 @@ def follow_target():
         search_left_right()
 
 
-def drop_lift():
-    ev3.speaker.say("Drop")
-    lift_motor.run_angle(150, -90)
-    wait(300)
-
-
-def raise_lift():
-    ev3.speaker.say("Lift")
-    lift_motor.run_angle(150, 90)
-    wait(300)
-
-
-def push_release_tool():
-    robot.straight(60)
-    wait(300)
-
-    robot.straight(-70)
-    wait(300)
-
-
-def reverse_with_items():
-    ev3.speaker.say("Reverse")
-    robot.straight(-500)
-    robot.stop()
-
-
-def collect_items():
-    ev3.speaker.say("Collect")
-
-    drop_lift()
-
-    for count in range(3):
-        push_release_tool()
-
-    ev3.speaker.say("Return")
-    reverse_with_items()
-
-    raise_lift()
-
-    ev3.speaker.say("Done")
-
-
-# -------------------------
-# MAIN PROGRAM
-# -------------------------
-
 ev3.speaker.say("Press")
+
+while not touch_sensor.pressed():
+    wait(10)
+
+wait(700)
+follow_mode = True
+ev3.speaker.say("Follow")
 
 while True:
 
-    # -------------------------
-    # BUTTON CONTROL
-    # -------------------------
-
-    if touch_sensor.pressed():
-
-        press_count += 1
-
-        # PRESS 1: start follow mode
-        if press_count == 1:
-            follow_mode = True
-            ev3.speaker.say("Follow")
-
-        # PRESS 2: stop and prepare to collect
-        elif press_count == 2:
-            follow_mode = False
-            robot.stop()
-            ev3.speaker.say("Press again")
-
-        # PRESS 3: collect items and reverse back
-        elif press_count == 3:
-            follow_mode = False
-            robot.stop()
-
-            collect_items()
-
-            press_count = 0
-            follow_mode = False
-
-        while touch_sensor.pressed():
-            wait(10)
-
-        wait(500)
-
-    # -------------------------
-    # FOLLOW LOGIC
-    # -------------------------
-
     if follow_mode:
         follow_target()
-    else:
-        robot.stop()
 
     wait(200)
